51fae0fba528581b72c7cac9b8d9c28cb0783bd0
Ashley Gould <agould@ucop.edu>
2019-07-02 08:14:16 -0700

    cli.taskrunner: --exec flag is comming along well
    it's all working now, but I'm still confused about passing args, kwargs to payloads

--- orgcrawler/cli/taskrunner.py
+++ orgcrawler/cli/taskrunner.py
@@ -19,7 +19,7 @@ from orgcrawler import tasks
 
 
 @click.command(context_settings=dict(help_option_names=['-h', '--help']))
-@click.option('--write-access-role', '-r',
+@click.option('--readwrite-role', '-r',
     help='IAM role to assume for executing write actions in accounts.'
 )
 @click.option('--task-spec-file', '-f',
@@ -33,17 +33,18 @@ from orgcrawler import tasks
     is_flag=True,
     help='If unset, all tasks will execute in "DryRun" mode.',
 )
-def main(write_access_role, task_spec_file, execute):
+def main(readwrite_role, task_spec_file, execute):
     '''
     Usage:
 
-      taskrunner -r MyIamRole -f /path/to/task-spec.yaml
-      taskrunner -r MyIamRole -f /path/to/task-spec.yaml --exec
+      taskrunner -f /path/to/task-spec.yaml
+
+      taskrunner -f /path/to/task-spec.yaml -r MyIamRole --exec
 
     '''
-    print(write_access_role)
-    print(task_spec_file)
-    print(execute)
+    #print(readwrite_role)
+    #print(task_spec_file)
+    #print(execute)
 
     task_spec = tasks.validate_task_spec(task_spec_file)
     org_access_role = task_spec['readonly_role']
@@ -58,26 +59,30 @@ def main(write_access_role, task_spec_file, execute):
     org.load()
 
     for task in task_spec['tasks']:
-        if not task['readonly'] and execute:
-            account_access_role = write_access_role
+        #print(yamlfmt(task))
+        if execute:
+            account_access_role = readwrite_role
         crawler = orgcrawler.crawlers.Crawler(
             org,
             access_role=account_access_role, 
             regions=task['regions'],
             accounts=task['accounts'],
         )
-        #print(yamlfmt(task))
-        #crawler.update_regions(task['regions'])
-        #crawler.update_accounts(task['accounts'])
-        #print(crawler.get_regions())
+        crawler.load_account_credentials()
         payload = get_payload_function_from_string(task['payload'])
 
-        if not task['readonly']:
-            task['data']['dryrun'] = not execute
-        if task['data'] is not None:
-            print(yamlfmt(task['data']))
-            execution = crawler.execute(payload, task['data'])
-        else:
-            execution = crawler.execute(payload)
+        try:
+            i = task['args'].index('dryrun')
+            task['args'][i] = not execute
+        except ValueError:
             pass
-        print(yamlfmt(execution.dump()))
+        except AttributeError:
+            pass
+
+
+        if 'args' not in task or task['args'] is None:
+            task['args'] = []
+        if 'kwargs' not in task or task['kwargs'] is None:
+            task['kwargs'] = {}
+        execution = crawler.execute(payload, task['args'], task['kwargs'])
+        click.echo(jsonfmt(execution.dump()))
